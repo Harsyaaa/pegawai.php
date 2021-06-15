c475c422f331cf13288bbaeeae631d371bbc4875
Harsyaaa <56983820+Harsyaaa@users.noreply.github.com>
2021-06-15 11:17:36 +0700

    update kepegawaian.php

---
+++ seshar2/kepegawaian.php
@@ -0,0 +1,20 @@
+<?php
+jhdbcihbsdcbsdbcidsbd[switch (dscnlsdodsjlbd
+	k
+	sd';b ljdsbpidsvnds
+	newt_listbox_set_data(n'd;skbsdvo;'k
+		v';psdj v'
+		sdvk
+		sdv ;jsbdv'
+		sdvnbo]sdv[]sd
+		v[n
+		sdp, num, data)) {
+	case 'value':
+		# code...
+		break;
+	
+	default:
+		# code...
+		break;
+}] 
+ ?>
